33b17730d3d0a672da99339eeacb83402f50a52a
Chau Tung Lam Nguyen <nctl144@gmail.com>
2019-02-07 16:35:47 -0500

    init fullpage:

--- js/main.js
+++ js/main.js
@@ -1,73 +1,82 @@
 (function($) {
-    function particleInit() {
-      // only enable particlesjs if the container is presented
-      if ($('#particles-js').length != 0) {
-        particlesJS.load('particles-js', '/particles.json', function() {
-            console.log("particles js loaded");
+  function particleInit() {
+    // only enable particlesjs if the container is presented
+    if ($('#particles-js').length != 0) {
+      particlesJS.load('particles-js', '/particles.json', function() {
+          console.log("particles js loaded");
+      });
+    }
+  }
+
+  function scrollInit() {
+    $("a").on('click', function(event) {
+      if (this.hash !== "" && $(this.hash).length != 0) {
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
         });
       }
-    }
+    });
+  }
 
-    function scrollInit() {
-      $("a").on('click', function(event) {
-        if (this.hash !== "" && $(this.hash).length != 0) {
-          event.preventDefault();
-
-          var hash = this.hash;
-
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-            window.location.hash = hash;
-          });
-        }
-      });
-    }
-
-    function mmenu() {
-      $("#navbar").mmenu({
-        "iconbar": {
-          "add": true,
-          "top": [
-            '<a href="https://www.facebook.com/PlattsHack/"><i class="fab fa-facebook-square"></i></a>',
-            '<a href="mailto:plattshack@gmail.com?Subject=Hello%20Plattshacks"><i class="far fa-envelope"></i></a>'
-          ]
-        },
-      	extensions: ["fx-menu-slide", "pagedim-black", "theme-dark", "position-right"]
-      });
-
-      var API = $("#navbar").data("mmenu");
-
-      $("#menu-mobile-button").click(function() {
-        API.open();
-      });
-    }
-
-    function loadBackground() {
-      setTimeout(function() {
-        $('.hero').css({
-          'opacity': '1'
-        });
-      }, 500);
-    }
-
-    function masonryInit() {
-      if ($('.grid').length != 0) {
-        $('.grid').masonry({
-          itemSelector: '.grid-item',
-          columnWidth: '.grid-sizer',
-          percentPosition: true,
-          gutter: 15
-        });
-      }
-    }
-
-    $(document).ready(function(){
-        particleInit();
-        scrollInit();
-        mmenu();
-        loadBackground();
-        masonryInit();
+  function mmenu() {
+    $("#navbar").mmenu({
+      "iconbar": {
+        "add": true,
+        "top": [
+          '<a href="https://www.facebook.com/PlattsHack/"><i class="fab fa-facebook-square"></i></a>',
+          '<a href="mailto:plattshack@gmail.com?Subject=Hello%20Plattshacks"><i class="far fa-envelope"></i></a>'
+        ]
+      },
+    	extensions: ["fx-menu-slide", "pagedim-black", "theme-dark", "position-right"]
     });
 
+    var API = $("#navbar").data("mmenu");
+
+    $("#menu-mobile-button").click(function() {
+      API.open();
+    });
+  }
+
+  function loadBackground() {
+    setTimeout(function() {
+      $('.hero').css({
+        'opacity': '1'
+      });
+    }, 500);
+  }
+
+  function masonryInit() {
+    if ($('.grid').length != 0) {
+      $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+        gutter: 15
+      });
+    }
+  }
+
+  function fullpageInit() {
+    new fullpage('#fullpage', {
+    	autoScrolling:true,
+    	scrollHorizontally: true
+    });
+
+    fullpage_api.setAllowScrolling(false);
+  }
+
+  $(document).ready(function(){
+    fullpageInit();
+    particleInit();
+    scrollInit();
+    mmenu();
+    loadBackground();
+    masonryInit();
+  });
 })(jQuery);
